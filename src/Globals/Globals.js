1fbc2e6acd5ed926a62a77e5bfabd582398d0e29
Kevin Castejon <pitikev1@hotmail.com>
2019-11-15 18:40:24 +0100

    updated logo and api url

--- src/Globals/Globals.js
+++ src/Globals/Globals.js
@@ -1,5 +1,6 @@
 export default class Globals {
 }
-Globals.APIURL = 'http://localhost:3000/';
+Globals.APIURL = 'https://vps735892.ovh.net:3000/';
+Globals.baseURL = '/demos/instruments-manager';
 // Globals.APIURL = 'https://omaracuja-api.herokuapp.com/';
 // Globals.ROUTEPREFIX = '';
